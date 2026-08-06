5cce253ac9ad2884f34e8ff93d825b8e31d94228
marianenatalsiqueira <mariane.natal.siqueira@escola.pr.gov.br>
2026-08-06 12:14:20 -0300

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+//Botão curtida 
+
+ document.addEventListener("DOMContentLoaded",()=>{
+    const likeBtn =ddocument.querySelector (".left-actions .action.btn:first-child");
+    if(!likeBtn) return;
+    const likeSvg=likeBtn.querySelector("svg");
+
+}
